9f7d670080f9d6e42f56a1bc34ccb66da52dab73
Yash Gurjar <ys.gurjar@gmail.com>
2023-05-06 15:39:38 +0000

    feat: make slider responsive to level selected

--- assets/js/levels-script.js
+++ assets/js/levels-script.js
@@ -1,8 +1,30 @@
+//initialise global objects
+
+levelData={
+    1:{
+        memory:1,
+        accuracy:1,
+        speed:1,
+
+        cards:16,
+        flipsMax:999,
+        timeMax:"59:59"
+    },
+    2:{
+        memory:1,
+        accuracy:2,
+        speed:3,
+
+        cards:16,
+        flipsMax:999,
+        timeMax:"30:00"
+    }
+}
+
 /**
  * The function resets the background color on level selector and make sure only one level is selected
  */
-
-function resetBg(selectedLevel) {
+function resetBg(selectedLevelElement) {
 
     //First, reset bg color to yellow on all level selectors
     let levels = document.getElementsByClassName("level-selector");
@@ -12,7 +34,23 @@ function resetBg(selectedLevel) {
         element.style.backgroundColor = '#ffdd92'; //resetting color
     }
     // Second, set the light blue color only on selected level
-    selectedLevel.style.backgroundColor = "#5a9cc6"; //set background for selected level
+    selectedLevelElement.style.backgroundColor = "#5a9cc6"; //set background for selected level
+}
+
+/**
+ * The function resets the slider to represent current level stats
+ */
+function resetSlider(selectedLevel) {
+    
+    //Get values
+    memory=String(levelData[selectedLevel]["memory"]);
+    accuracy=String(levelData[selectedLevel]["accuracy"]);
+    speed=String(levelData[selectedLevel]["speed"]);
+
+    //Set values
+    document.getElementById("memory-slider").setAttribute("value", memory);
+    document.getElementById("accuracy-slider").setAttribute("value", accuracy);
+    document.getElementById("speed-slider").setAttribute("value", speed);
 }
 
 //Get HTMl collection of level selector
@@ -24,7 +62,7 @@ for (let index = 0; index < levels.length; index++) {
     const levelSelected = parseInt(levelElement.innerText);
     levelElement.addEventListener('click', function () {
         resetBg(levelElement);          //clears bg from other levels and set bg on selected level
-        // resetSlider(levelSelected);     //set slider level according to selected level
+        resetSlider(levelSelected);     //set slider level according to selected level
         // resetRules(levelSelected);      //set rules according to selected level
         document.getElementById("level-id-text-on-CTA").innerText = levelElement.innerText; //changing innertext of CTA button;
     })
