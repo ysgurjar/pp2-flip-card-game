a618277fe5f522e7ffda377de97571b30dd3be15
Yash Gurjar <ys.gurjar@gmail.com>
2023-05-12 23:47:58 +0000

    feat: add function that dynamically creates div elements that works as card

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -8,3 +8,20 @@ document.getElementById("game-stats").style.display="block";
 document.getElementById("game-stats").style.textAlign="center";
 
 document.getElementById("title").style.display="none";
+
+document.body.onload = addElement;
+
+function addElement() {
+    // create a new div element
+    const newDiv = document.createElement("div");
+  
+    // and give it some content
+    const newContent = document.createTextNode("Hi there and greetings!");
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+    debugger;
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("cards-wrapper");
+    currentDiv.appendChild(newDiv);
+  }
