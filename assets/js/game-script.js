99cfe0e3774a342d49e0b8127450979c7510084c
Yash Gurjar <ys.gurjar@gmail.com>
2023-05-13 04:11:08 +0000

    feat:allow only two cards to flip

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -10,27 +10,27 @@ document.getElementById("game-stats").style.textAlign = "center";
 document.getElementById("title").style.display = "none";
 
 
-    // dynamically create the card elements
-    for (let index = 0; index < 10; index++) {
-        addElement();
-    }
+// dynamically create the card elements
+for (let index = 0; index < 10; index++) {
+    addElement();
+}
 
 function addElement() {
     // create a new div element
     const newDiv = document.createElement("div");
-    newDiv.setAttribute("class","card2");
-    
+    newDiv.setAttribute("class", "card2");
+
     // create two child divs for front and back of card
-    const front=document.createElement("div");
-    front.setAttribute("class","front");
-    const back=document.createElement("div");
-    back.setAttribute("class","back");
+    const front = document.createElement("div");
+    front.setAttribute("class", "front");
+    const back = document.createElement("div");
+    back.setAttribute("class", "back");
 
     // add text to child divs
 
-    frontText=document.createTextNode("Front side");
-    backText=document.createTextNode("Back side")
-    
+    frontText = document.createTextNode("Front side");
+    backText = document.createTextNode("Back side")
+
     front.appendChild(frontText);
     back.appendChild(backText);
 
@@ -43,15 +43,56 @@ function addElement() {
     currentDiv.appendChild(newDiv);
 }
 
-cards=document.querySelectorAll(".card2");
+cards = document.querySelectorAll(".card2");
 
 cards.forEach(element => {
-    element.addEventListener('click',function () {
-        flipCard(element);
+    element.addEventListener('click', function () {
+
+        //run click counter, returns no of cards flipped
+        let cardsFlipped=clickCounter();
+        console.log(cardsFlipped);
+        //flip the card if the card is not already flipped and it is one of the first two cards
+        flipCard(element,cardsFlipped);
+        debugger;
+        //flow if two cards are flipped
+        if (cardsFlipped==2) {
+            twoCardsFlipped();
+        }
     })
 });
 
-function flipCard(element) {
+/**
+ * flips card after checking necessary conditions
+ */
+
+clickOnCards = 0;
+
+function flipCard(element, cardsFlipped) {
+
     
-    element.classList.toggle("flipCard");
+    //check if card already flipped?
+    isFlipped = element.classList.contains("flipcard");
+
+    //is it first or second card to be flipped?
+    isFirstorSecondCard = (cardsFlipped < 3)
+
+    if ((!isFlipped) && (isFirstorSecondCard)) {
+
+        // flip the card
+        element.classList.toggle("flipCard");
+    }
+
+}
+
+
+/**
+ * counts clicks on card
+ */
+function clickCounter() {
+    
+    return ++clickOnCards;
+}
+
+function twoCardsFlipped (){
+    console.log("two cards flipped");
 }
